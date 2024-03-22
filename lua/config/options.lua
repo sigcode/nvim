548bff12655ce30e3d53b62572c54ab7a556abc4
Simon Günther <simon.guenther@sucurema.com>
2024-03-22 14:21:14 +0100

    ...

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -33,6 +33,7 @@ vim.opt.splitright = true -- Put new windows right of current
 vim.opt.splitkeep = "cursor"
 vim.opt.mouse = "a"
 vim.opt.swapfile = false
+vim.opt.conceallevel = 2
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
